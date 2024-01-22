12fd10582f6fe9b058679bae87d05911f0900e2a
Karra <karra.maynard@gmail.com>
2024-01-22 17:02:58 -0500

    create department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
