582a6f07293ef9082eb6de316469562b32c6a803
Karra <karra.maynard@gmail.com>
2024-01-25 22:59:43 -0500

    add connection credentials and export the function

--- config/connect.js
+++ config/connect.js
@@ -2,3 +2,12 @@ import dotenv from 'dotenv'
 import mysql from 'mysql2';
 
 dotenv.config();
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+export default connection;
