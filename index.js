54d8a712e69234fed35a344b62c651565c89bbdf
Karra <karra.maynard@gmail.com>
2024-01-26 00:12:33 -0500

    create connection to server

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import connection from './config/connect.js';
 import inquirer from 'inquirer';
-// TODO: import helper functions
-
+// Import functions
+import { getDepartments, getRoles } from './helpers/queries.js';
 
 // TODO: connect to database
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to database', error);
+    return;
+  } else {
+    console.log('Successfully connected to database');
+    // Run application
+    main();
+  }
+});
+
 
 // TODO: create inquirer prompts
