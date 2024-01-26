754efaf401ee7286303cbc844514157f92d5fdfa
Karra <karra.maynard@gmail.com>
2024-01-26 00:06:55 -0500

    some todos

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import connection from './config/connect.js';
 import inquirer from 'inquirer';
+// TODO: import helper functions
+
+
+// TODO: connect to database
+
+// TODO: create inquirer prompts
