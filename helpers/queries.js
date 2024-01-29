6169471f7bf4f20535186ce83725d1f1a4c4b275
Karra <karra.maynard@gmail.com>
2024-01-29 01:19:06 -0500

    import main and inquirer

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,5 +1,8 @@
 import db from '../config/connect.js';
+import main from '../index.js';
+import inquirer from 'inquirer';
 
+// * Get All *
 export async function getDepartments() {
   console.log('hello')
 }
