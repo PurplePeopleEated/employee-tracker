ec8a01a1dc374ca69863a7ccceb519c899c8bc70
Karra <karra.maynard@gmail.com>
2024-01-29 01:27:01 -0500

    add logic to get Departments

--- helpers/queries.js
+++ helpers/queries.js
@@ -2,7 +2,13 @@ import db from '../config/connect.js';
 import main from '../index.js';
 import inquirer from 'inquirer';
 
-// * Get All *
+// * Get *
 export async function getDepartments() {
-  console.log('hello')
-}
+  try {
+    const [departments] = await db.promise().query('SELECT * FROM department');
+    console.log('Departments:', departments);
+    main();
+  } catch (error) {
+    console.error('Error fetching departments:', error);
+  }
+};
